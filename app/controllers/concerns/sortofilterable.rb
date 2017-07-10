c172a5a80f5c9a1e7b05cc6abf929084ad6f5bb0
Rinat Garifullin <ringarifullin@gmail.com>
2017-07-10 15:07:33 +0300

    Separate methods from included block in controller concerns

--- app/controllers/concerns/sortofilterable.rb
+++ app/controllers/concerns/sortofilterable.rb
@@ -2,47 +2,45 @@
 module Sortofilterable
   extend ActiveSupport::Concern
 
-  included do
-    private
+  private
 
-    def filter_params
-      []
-    end
+  def filter_params
+    []
+  end
 
-    def sort_params
-      %i[sort_by direction]
-    end
+  def sort_params
+    %i[sort_by direction]
+  end
 
-    def pagination_params
-      %i[page]
-    end
+  def pagination_params
+    %i[page]
+  end
 
-    def set_default_sort_opts
-      set_default_sort_field
-      set_default_sort_direction
-    end
+  def set_default_sort_opts
+    set_default_sort_field
+    set_default_sort_direction
+  end
 
-    def set_default_sort_field
-      @default_sort_field = { nil => :id }
-    end
+  def set_default_sort_field
+    @default_sort_field = { nil => :id }
+  end
 
-    def set_default_sort_direction
-      @default_sort_direction = { nil => :asc }
-    end
+  def set_default_sort_direction
+    @default_sort_direction = { nil => :asc }
+  end
 
-    def sort_field(scope = nil)
-      params.dig(scope, :sort_by) || params[:sort_by] ||
-        @default_sort_field[scope]
-    end
+  def sort_field(scope = nil)
+    params.dig(scope, :sort_by) || params[:sort_by] ||
+      @default_sort_field[scope]
+  end
 
-    def sort_direction(scope = nil)
-      params.dig(scope, :direction) || params[:direction] ||
-        @default_sort_direction[scope]
-    end
+  def sort_direction(scope = nil)
+    params.dig(scope, :direction) || params[:direction] ||
+      @default_sort_direction[scope]
+  end
 
-    def permit_params
-      @permitted_params = params.permit(sort_params, filter_params,
-                                        pagination_params)
-    end
+  def permit_params
+    @permitted_params = params.permit(sort_params, filter_params,
+                                      pagination_params)
   end
 end
--- app/controllers/concerns/url_restorable.rb
+++ app/controllers/concerns/url_restorable.rb
@@ -2,42 +2,39 @@
 module UrlRestorable
   extend ActiveSupport::Concern
 
-  included do
-    private
+  private
 
-    def restore_url_params(key_prefix)
-      key = "#{key_prefix}_url_params"
-      if @permitted_params
-        params_wo_paging = @permitted_params.reject do |k, _|
-          pagination_params.include?(k.to_sym)
-        end
+  def restore_url_params(key_prefix)
+    key = "#{key_prefix}_url_params"
+    if @permitted_params
+      params_wo_paging = @permitted_params.reject do |k, _|
+        pagination_params.include?(k.to_sym)
       end
-      all_params = params_wo_paging ||
-                   params.permit(filter_params + sort_params)
+    end
+    all_params = params_wo_paging || params.permit(filter_params + sort_params)
 
-      if params[:reset_url]
-        if params[:reset_url] == 'all'
-          session[key] = nil
-        else
-          session[key] = Rack::Utils.parse_nested_query(session[key])
-                                    .except(params[:reset_url]).to_query
-        end
-      elsif session[key].blank?
-        session[key] = all_params.to_h.to_query
+    if params[:reset_url]
+      if params[:reset_url] == 'all'
+        session[key] = nil
+      else
+        session[key] = Rack::Utils.parse_nested_query(session[key])
+                                  .except(params[:reset_url]).to_query
       end
-
-      return if session[key].blank?
-
-      new_params = Rack::Utils.parse_nested_query(session[key])
-                              .merge(deep_transform(all_params.to_h))
-      session[key] = new_params.to_query
-      params.merge!(new_params)
+    elsif session[key].blank?
+      session[key] = all_params.to_h.to_query
     end
 
-    def deep_transform(hash)
-      hash.transform_values do |val|
-        val.is_a?(String) ? val : val.transform_values { |vv| vv }
-      end
+    return if session[key].blank?
+
+    new_params = Rack::Utils.parse_nested_query(session[key])
+                            .merge(deep_transform(all_params.to_h))
+    session[key] = new_params.to_query
+    params.merge!(new_params)
+  end
+
+  def deep_transform(hash)
+    hash.transform_values do |val|
+      val.is_a?(String) ? val : val.transform_values { |vv| vv }
     end
   end
 end
